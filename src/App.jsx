4cefcf9ee59b6b23abe4d551b716e3656b83affd
Scott_Lu_Bo_Xuan <apc582nntscott@arch.nycu.edu.tw>
2025-12-22 08:18:05 +0800

    deploy5

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,5 @@
 import { useState } from 'react'
-// 1. 引入 React Router 必要元件
+// 修正：統一使用 HashRouter
 import { HashRouter as Router, Routes, Route } from 'react-router-dom';
 
 import './App.css'
@@ -10,31 +10,29 @@ import Layout from './PageLayout/Layout'
 import KnowledgeGraphPage from './PageLayout/KnowledgeGraphPage'
 import { GraphProvider } from './components/KnowledgeGraph/GraphProvider'
 
-// 實際上你應該會在 src/pages/ 下建立這些檔案
+// 頁面元件定義
 const EducationPage = () => <div className="p-20 text-fg min-h-screen bg-surface">這是文化教案頁面</div>
 const WorkshopPage = () => <div className="p-20 text-fg min-h-screen bg-surface">這是共創工作坊頁面</div>
 
-
 function App() {
   return (
-    <BrowserRouter>
+    // 將 BrowserRouter 改為 Router (即上面引入的 HashRouter)
+    <Router>
       <Layout>
-          <GraphProvider>
+        {/* 將 Provider 放在 Layout 內，這樣所有頁面都能共享資料 */}
+        <GraphProvider>
           <Routes>
-            {/* 3. 設定首頁 */}
+            {/* 設定首頁 */}
             <Route path="/" element={<HomePage />} />
 
-            {/* 4. 設定路徑 */}
+            {/* 設定路徑 */}
             <Route path="/KnowledgeGraphPage" element={<KnowledgeGraphPage />} />
             <Route path="/education" element={<EducationPage />} />
             <Route path="/workshop" element={<WorkshopPage />} />
-            
-            {/* 之後可以繼續新增更多頁面 */}
-            {/* <Route path="/about" element={<AboutPage />} /> */}
           </Routes>
-          </GraphProvider>
+        </GraphProvider>
       </Layout>
-    </BrowserRouter>
+    </Router>
   )
 }
 
